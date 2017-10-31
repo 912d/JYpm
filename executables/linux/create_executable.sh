1f898a9122908532e91802003ba6442391b03a72
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-10-31 23:14:29 +0100

    Added prototype bash script that creates linux executable.

---
+++ executables/linux/create_executable.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+## Run this to create executable
+cat stub.sh jypm.jar > jypm
+chmod +x jypm
---
+++ executables/linux/stub.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+MYSELF=`which "$0" 2>/dev/null`
+MYSELF="${MYSELF/' '/'\ '}"
+cat $MYSELF > /dev/null;
+[ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
+java=java
+if test -n "$JAVA_HOME"; then
+    java="$JAVA_HOME/bin/java"
+fi
+exec "$java" -Xms64m -Xmx128m -jar $MYSELF "$@"
+exit 1
