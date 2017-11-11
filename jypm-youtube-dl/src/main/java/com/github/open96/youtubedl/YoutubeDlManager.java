d055254319114b5c9e4f99b982dc8075ee223de7
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-11-11 21:50:25 +0100

    Moved deleting youtube-dl directory to separate method.

--- jypm-youtube-dl/src/main/java/com/github/open96/youtubedl/YoutubeDlManager.java
+++ jypm-youtube-dl/src/main/java/com/github/open96/youtubedl/YoutubeDlManager.java
@@ -127,6 +127,20 @@ public class YoutubeDlManager {
     }
 
     /**
+     * Delete recursively directory in which youtube-dl is stored.
+     */
+    public void deletePreviousVersionIfExists() {
+        File youtubeDlDirectory = new File(YOUTUBE_DL_DIRECTORY);
+        if (youtubeDlDirectory.exists() && youtubeDlDirectory.listFiles() != null) {
+            for (File f : youtubeDlDirectory.listFiles()) {
+                f.delete();
+            }
+        }
+        youtubeDlDirectory.delete();
+        youtubeDlDirectory.mkdir();
+    }
+
+    /**
      * Utilizing methods in this class this method checks for youtube-dl update and appends it if needed.
      */
     public void downloadYoutubeDl() {
@@ -157,14 +171,7 @@ public class YoutubeDlManager {
                     downloadLink = new URL(asset.getBrowserDownloadUrl());
 
                     //Clean youtube-dl directory before making any changes to it
-                    File youtubeDlDirectory = new File(YOUTUBE_DL_DIRECTORY);
-                    if (youtubeDlDirectory.exists() && youtubeDlDirectory.listFiles() != null) {
-                        for (File f : youtubeDlDirectory.listFiles()) {
-                            f.delete();
-                        }
-                    }
-                    youtubeDlDirectory.delete();
-                    youtubeDlDirectory.mkdir();
+                    deletePreviousVersionIfExists();
 
                     //Download youtube-dl
                     ReadableByteChannel readableByteChannel = Channels.newChannel(downloadLink.openStream());
