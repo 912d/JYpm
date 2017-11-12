98ebe676197fbbd18487699a6c93011fc436df0e
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-11-12 12:06:39 +0100

    jypm-updater no longer depends on jypm-fxml and no longer creates GUI elements. Instead, everything related to that is done in jypm-fxml.

--- jypm-updater/src/main/java/com/github/open96/updater/Updater.java
+++ jypm-updater/src/main/java/com/github/open96/updater/Updater.java
@@ -3,25 +3,13 @@ package com.github.open96.updater;
 import com.github.open96.api.github.GitHubApiClient;
 import com.github.open96.api.github.GitHubApiEndpointInterface;
 import com.github.open96.api.github.pojo.release.ReleaseJSON;
-import com.github.open96.fxml.DialogWindowController;
-import com.github.open96.settings.OS_TYPE;
 import com.github.open96.settings.SettingsManager;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 
-import java.awt.*;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Properties;
 
 public class Updater {
@@ -88,26 +76,12 @@ public class Updater {
         if (releaseJSON != null) {
             if (!releaseJSON.getTagName().equals(runtimeVersion)) {
                 log.trace("New update available\nCURRENT VERSION: " + runtimeVersion + "\nNEW VERSION: " + releaseJSON.getTagName());
-                try {
-                    alertUser();
-                } catch (IOException e) {
-                    log.error("Error during setting up update window", e);
-                } catch (ExceptionInInitializerError | NoClassDefFoundError e) {
-                    log.error("No stage found", e);
-                }
                 return releaseJSON.getTagName();
             }
         } else {
             log.error("API object is empty!", new IllegalStateException("API object is empty!"));
         }
         return null;
-    }
-
-    /**
-     * @return Version of JYpm that is currently running
-     */
-    private String getRuntimeVersion() {
-        return runtimeVersion;
     }
 
     /**
@@ -138,51 +112,11 @@ public class Updater {
         }
     }
 
-
-    //TODO Integrate code from this method into jypm-fxml to break circular dependency between jypm-fxml and jypm-updater
-    private void alertUser() throws IOException {
-
-        //Create message
-        StringBuilder messageBuilder = new StringBuilder();
-        messageBuilder.append("Update available").append("\n");
-        messageBuilder.append(getRuntimeVersion()).append(" ").append("(Current)").append(" --> ")
-                .append(releaseJSON.getTagName()).append(" ").append("(").append(releaseJSON.getPublishedAt().substring(0, 10)).append(")")
-                .append("\n");
-        messageBuilder.append("Visit").append(" ").append(releaseJSON.getHtmlUrl()).append(" ").append("for changelog and more details.")
-                .append("\n");
-
-        //Windows users have executables, so they have to visit GitHub and download it manually for the time being
-        String positiveButtonText = "Visit GitHub";
-        String negativeButtonText = "Later";
-
-        EventHandler<ActionEvent> positiveButtonEventHandler = event -> {
-            if (Desktop.isDesktopSupported()) {
-                try {
-                    if (SettingsManager.getInstance().getOS() == OS_TYPE.OPEN_SOURCE_UNIX) {
-                        Runtime.getRuntime().exec("xdg-open " + releaseJSON.getHtmlUrl(), null);
-                    } else if (SettingsManager.getInstance().getOS() == OS_TYPE.WINDOWS) {
-                        Desktop.getDesktop().browse(new URI(releaseJSON.getHtmlUrl()));
-                    }
-
-                } catch (IOException | URISyntaxException e) {
-                    log.error(e);
-                } catch (UnsupportedOperationException e) {
-                    log.error("Browsing is not supported on this system");
-                }
-            }
-        };
-
-        Stage subStage = new Stage();
-        subStage.setTitle("Update available");
-        subStage.getIcons().add(new Image("/icon/launcher-128-128.png"));
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/dialogWindow.fxml"));
-        Parent root = fxmlLoader.load();
-        DialogWindowController controller = fxmlLoader.getController();
-        controller.setData(messageBuilder.toString(), positiveButtonText, negativeButtonText, positiveButtonEventHandler);
-        Scene scene = new Scene(root);
-        subStage.setScene(scene);
-        subStage.show();
-        subStage.setAlwaysOnTop(true);
-        subStage.requestFocus();
+    /**
+     * @return JSON response from github casted to object
+     */
+    public ReleaseJSON getJSONObject() {
+        return releaseJSON;
     }
+
 }
