7e2dbb50dabac64844cf4c7cd5695fc66baa8dd6
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-12-08 08:31:13 +0100

    Created test for convertDirectory() up to the moment when playlist has been downloaded to local storage.

--- jypm-test/src/test/java/com/github/open96/jypm/ffmpeg/FfmpegManagerTest.java
+++ jypm-test/src/test/java/com/github/open96/jypm/ffmpeg/FfmpegManagerTest.java
@@ -1,22 +1,54 @@
 package com.github.open96.jypm.ffmpeg;
 
+import com.github.open96.jypm.download.DownloadManager;
+import com.github.open96.jypm.playlist.PlaylistManager;
+import com.github.open96.jypm.playlist.QUEUE_STATUS;
+import com.github.open96.jypm.playlist.pojo.Playlist;
 import com.github.open96.jypm.settings.SettingsManager;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.reflect.Field;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class FfmpegManagerTest {
     //IMPORTANT - IF YOU USE DOWNLOADED EXECUTABLE YOU NEED TO CHANGE THIS VARIABLE
     private static final String PATH_TO_FFMPEG = "ffmpeg";
+    private static final String SAMPLE_PLAYLIST_LINK = "PLK1OE0wPYodKbHOX0pd5nD4TTT9CflG39";
+    private static String playlistPath;
 
+    @BeforeClass
+    public static void initialize() {
+        setPlaylistPath();
+        deleteConfigFiles();
+        PlaylistManager.getInstance();
+        assertEquals(0, PlaylistManager.getInstance().getPlaylists().size());
+        DownloadManager.getInstance();
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        File playlistDirectory = new File("playlist_dir/");
+        ensurePlaylistDirectoryIsPresentAndEmpty();
+        playlistDirectory.delete();
+    }
+
+    public static void setPlaylistPath() {
+        File playlistDirectory = new File("playlist_dir/");
+        playlistPath = playlistDirectory.getAbsolutePath();
+        ensurePlaylistDirectoryIsPresentAndEmpty();
+    }
+
+    public static void deleteConfigFiles() {
+        File settingsJSON = new File("settings.json");
+        File playlistsJSON = new File("playlists.json");
+        settingsJSON.delete();
+        playlistsJSON.delete();
+    }
 
     @Before
     public void resetSingleton() {
@@ -52,6 +84,26 @@ public class FfmpegManagerTest {
         assertTrue(FfmpegManager.getInstance().checkIfExecutableIsValid());
     }
 
+    @Test
+    public void testConvertDirectory() throws InterruptedException {
+        //Create playlist object and issue the download
+        Playlist testPlaylist = new Playlist(SAMPLE_PLAYLIST_LINK, playlistPath);
+        PlaylistManager.getInstance().add(testPlaylist);
+        while (PlaylistManager.getInstance().getPlaylistByLink(testPlaylist.getPlaylistLink()) == null) {
+            Thread.sleep(10);
+        }
+        DownloadManager.getInstance().download(testPlaylist);
+        while (testPlaylist.getStatus() == QUEUE_STATUS.DOWNLOADING
+                || testPlaylist.getStatus() == QUEUE_STATUS.QUEUED) {
+            Thread.sleep(500);
+            Integer downloadProgress = DownloadManager.getInstance().getDownloadProgress();
+            if (downloadProgress != null) {
+                System.out.println(downloadProgress + "/" + testPlaylist.getTotalVideoCount());
+            }
+        }
+        assertEquals(QUEUE_STATUS.DOWNLOADED, testPlaylist.getStatus());
+    }
+
 
     //TODO Create Util module and put this code and other universal methods in there
     private String getProcessOutput(Process process) throws IOException, InterruptedException {
@@ -69,5 +121,21 @@ public class FfmpegManagerTest {
             bufferedReader.close();
             return output.toString();
         }
+    }
+
+    public static void ensurePlaylistDirectoryIsPresentAndEmpty() {
+        File dir = new File(playlistPath);
+        //If file/directory with "playlist_dir" name is present, delete it
+        if (dir.exists()) {
+            if (dir.isDirectory()) {
+                for (File f : dir.listFiles()) {
+                    f.delete();
+                }
+            }
+            dir.delete();
+            assertFalse(dir.exists());
+        }
+        dir.mkdir();
+        assertTrue(dir.exists());
     }
 }
