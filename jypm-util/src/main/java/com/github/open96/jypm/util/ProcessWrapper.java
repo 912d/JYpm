faa4577dcd73f47e399e01b28a99b093bb2e3bb2
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-12-11 16:08:19 +0100

    getProcessOutput() now executes on separate thread.

--- jypm-util/src/main/java/com/github/open96/jypm/util/ProcessWrapper.java
+++ jypm-util/src/main/java/com/github/open96/jypm/util/ProcessWrapper.java
@@ -1,9 +1,14 @@
 package com.github.open96.jypm.util;
 
+import com.github.open96.jypm.thread.TASK_TYPE;
+import com.github.open96.jypm.thread.ThreadManager;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class ProcessWrapper {
     private Process process;
@@ -17,20 +22,29 @@ public class ProcessWrapper {
      *
      * @return String with process's output
      */
-    public String getProcessOutput() throws IOException, InterruptedException {
+    public String getProcessOutput() throws InterruptedException {
         //Create BufferedReader that will read process's output
-        try (InputStream inputStream = process.getInputStream()) {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-            StringBuilder output = new StringBuilder();
-            while ((line = bufferedReader.readLine()) != null || process.isAlive()) {
-                if (line != null) {
-                    output.append(line);
+        Callable<String> processOutputGetter = () -> {
+            try (InputStream inputStream = process.getInputStream()) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                StringBuilder output = new StringBuilder();
+                while ((line = bufferedReader.readLine()) != null || process.isAlive()) {
+                    if (line != null) {
+                        output.append(line);
+                    }
                 }
+                process.waitFor();
+                bufferedReader.close();
+                return output.toString();
             }
-            process.waitFor();
-            bufferedReader.close();
-            return output.toString();
+        };
+        Future<String> processOutputFuture = ThreadManager.getInstance().sendTask(processOutputGetter, TASK_TYPE.OTHER);
+        try {
+            return processOutputFuture.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+        return null;
     }
 }
