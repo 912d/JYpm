00874cef3f62107026d05588b5a491e33abdef89
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-12-19 11:11:26 +0100

    Created triggerConversion() method.

--- jypm-fxml/src/main/java/com/github/open96/jypm/fxml/ConversionWindowController.java
+++ jypm-fxml/src/main/java/com/github/open96/jypm/fxml/ConversionWindowController.java
@@ -2,6 +2,11 @@ package com.github.open96.jypm.fxml;
 
 import com.github.open96.jypm.ffmpeg.FILE_EXTENSION;
 import com.github.open96.jypm.ffmpeg.FfmpegManager;
+import com.github.open96.jypm.playlist.PLAYLIST_STATUS;
+import com.github.open96.jypm.playlist.pojo.Playlist;
+import com.github.open96.jypm.thread.TASK_TYPE;
+import com.github.open96.jypm.thread.ThreadManager;
+import com.github.open96.jypm.tray.TrayIcon;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,12 +18,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ConversionWindowController implements Initializable {
 
     //Initialize log4j logger for later use in this class
     private static final Logger LOG = LogManager.getLogger(DetailsWindowController.class.getName());
+
+    //Variables needed to start conversion process
+    private Playlist playlist;
+    private List<Boolean> conversionProgress;
 
 
     @FXML
@@ -74,5 +84,39 @@ public class ConversionWindowController implements Initializable {
 
     public void onConvertButtonClick(ActionEvent actionEvent) {
 
+    }
+
+
+    private void triggerConversion() {
+        ThreadManager.getInstance().sendVoidTask(new Thread(() -> {
+            if (playlist.getStatus() == PLAYLIST_STATUS.DOWNLOADED) {
+                //Start conversion
+                playlist.setStatus(PLAYLIST_STATUS.CONVERTING);
+                conversionProgress = FfmpegManager
+                        .getInstance()
+                        .convertDirectory(playlist.getPlaylistLocation(), FILE_EXTENSION.MP3, 320);
+                //Wait until all videos have been converted
+                int convertedVideos = 0;
+                while (convertedVideos != conversionProgress.size()) {
+                    try {
+                        convertedVideos = conversionProgress
+                                .stream()
+                                .filter(conversionState -> conversionState.equals(Boolean.TRUE))
+                                .toArray()
+                                .length;
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        LOG.error("Thread sleep has been interrupted!");
+                    }
+                }
+                //Indicate that playlist has finished it's conversion
+                playlist.setStatus(PLAYLIST_STATUS.DOWNLOADED);
+            }
+            //Display notification from tray
+            if (TrayIcon.isTrayWorking()) {
+                TrayIcon.getInstance().displayNotification("Conversion finished",
+                        playlist.getPlaylistName() + " has been converted");
+            }
+        }), TASK_TYPE.CONVERSION);
     }
 }
