4ea43f01262124a327c6c9e630681963a71dc6e3
Wojciech Hołowiak <w.holowiak@gmail.com>
2017-12-20 10:48:41 +0100

    FfmpegManager conversion tasks no longer stop and do nothing when ThreadManager sets executionPermission to false.

--- jypm-ffmpeg/src/main/java/com/github/open96/jypm/ffmpeg/FfmpegManager.java
+++ jypm-ffmpeg/src/main/java/com/github/open96/jypm/ffmpeg/FfmpegManager.java
@@ -112,12 +112,12 @@ public class FfmpegManager {
                 ThreadManager.getInstance().sendVoidTask(new Thread(() -> {
                     try {
                         //Create command that will be issued via Runtime
-                        String command[] = createCommand(file.getName(), targetExtension, bitrate);
-                        if (command != null) {
-                            if (file.getName().endsWith(targetExtension.toString())) {
-                                LOG.trace(file.getName() + " - Conversion from to same format is pointless, skipping");
-                            } else {
-                                if (ThreadManager.getExecutionPermission()) {
+                        if (ThreadManager.getExecutionPermission()) {
+                            String command[] = createCommand(file.getName(), targetExtension, bitrate);
+                            if (command != null) {
+                                if (file.getName().endsWith(targetExtension.toString())) {
+                                    LOG.trace(file.getName() + " - Conversion from to same format is pointless, skipping");
+                                } else {
                                     //Run ffmpeg
                                     Process p = runtime.exec(command, null, targetDirectory);
                                     //Wait until it finishes
